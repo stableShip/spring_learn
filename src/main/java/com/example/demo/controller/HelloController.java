2e8ecd4733a6fed40b6bb3c5e9b6f1a4317b34c9
alex <296888680@qq.com>
2018-09-16 21:57:15 +0800

    add redis session

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -6,14 +7,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @RestController
 @EnableAutoConfiguration
 @Slf4j
 public class HelloController {
 
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.GET})
-    private String Hello() {
+    private String Hello(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("name", "JIE");
         log.info("Hello World!");
         return "Hello World!";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "getName")
+    private String getSessionName(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String name = (String) session.getAttribute("name");
+        return name;
     }
 }
--- src/test/java/com/example/demo/controller/HelloControllerTest.java
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -7,11 +7,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.http.HttpSession;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -26,8 +29,11 @@ public class HelloControllerTest {
     @Autowired
     private WebApplicationContext context;
 
+    private MockHttpSession session;
+
     @Before
     public void setUp() throws Exception {
+        session = new MockHttpSession();
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
     }
 
@@ -49,6 +55,22 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andReturn();
         assertEquals(result.getResponse().getContentAsString(), "Hello World!");
+    }
+
+    @Test
+    public void getName() throws Exception {
+        this.mockMvc.perform(
+                post("/hello").accept(MediaType.APPLICATION_JSON).session(session)
+        )
+                .andExpect(status().isOk())
+                .andReturn();
+        MvcResult result = this.mockMvc.perform(
+                post("/getName").accept(MediaType.APPLICATION_JSON).session(session)
+        )
+                .andExpect(status().isOk())
+                .andReturn();
+        HttpSession requestSession = result.getRequest().getSession();
+        assertEquals(requestSession.getAttribute("name"), "JIE");
     }
 
 }
