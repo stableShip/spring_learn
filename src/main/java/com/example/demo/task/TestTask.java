219c03e0bbdc66f0a8249a73e55d319599bab8d9
alex <296888680@qq.com>
2018-09-17 16:28:12 +0800

    add baseSchedule task

---
+++ src/main/java/com/example/demo/task/TestTask.java
@@ -0,0 +1,26 @@
+package com.example.demo.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class TestTask extends BaskTask {
+
+    public TestTask() {
+        this.nameZh = "测试任务";
+        this.name = "testTask";
+    }
+
+    @Scheduled(cron = "* */1 * * * *")
+    public void process() {
+        super.process();
+    }
+
+    @Override
+    void content() {
+        log.info("run testTask Content");
+    }
+
+}
