fd7de7a8272ecb09b72aad57a9e10cedc5a15487
alex <296888680@qq.com>
2019-04-16 21:38:16 +0800

    add spring async/future rest test

--- src/main/java/com/example/demo/controller/FeatureController.java
+++ src/main/java/com/example/demo/controller/FeatureController.java
@@ -3,22 +3,26 @@ package com.example.demo.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 @RestController
 @EnableAutoConfiguration
 @Slf4j
+@Async
 public class FeatureController {
 
     @GetMapping(value = "/mutliThreadCreadFile")
@@ -58,5 +62,29 @@ public class FeatureController {
         return MessageFormat.format("use time: {0} s", (finished - start) / 1000);
     }
 
+    @GetMapping(value = "/asyncCreateFileThenReturn")
+    private static CompletableFuture asyncCreateFileThenReturn() {
+        log.info("asyncCreateFileThenReturn request in");
+        long start = System.currentTimeMillis();
+        CompletableFuture future = CompletableFuture.supplyAsync(() -> {
+                    String filePath = "./test.text";
+                    try (FileOutputStream out = new FileOutputStream(filePath)) {
+                        out.write("Hello World".getBytes());
+                        Thread.sleep(10000);
+//                        throw new Exception("I am not happy");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    Map result = new HashMap<>();
+                    result.put("code", 200);
+                    result.put("data", filePath);
+                    long finished = System.currentTimeMillis();
+
+                    result.put("useTime", (finished - start) / 1000);
+                    return result;
+                }
+        );
+        return future;
+    }
 
 }
